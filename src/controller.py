84c23ee099ba7cdd3cb6d003dcd47a716aebfc55
Sergey Chernikov <schernikov@gmail.com>
2013-09-29 23:12:31 -0700

    fix lockup bug

--- src/controller.py
+++ src/controller.py
@@ -9,17 +9,6 @@ echo "rising" > /sys/class/gpio/gpio23/edge
 import time, threading
 import RPIO
 
-def on_click(pin, val):
-    global laststamp, tickcount
-    tickcount += 1
-    now = time.time()
-    diff = now-laststamp
-    if diff >= 0.1:
-        print "%.2f" % (tickcount/diff)
-        tickcount = 0
-        laststamp = now
-    # fast, slow, stop states
-
 class SensorControl(object):
     def __init__(self):
         inputpins = [23]
@@ -28,41 +17,59 @@ class SensorControl(object):
             def on_turn(pin, val): self.on_turn(pin, val)
             RPIO.add_interrupt_callback(pin, on_turn, pull_up_down=RPIO.PUD_DOWN, edge='rising', threaded_callback=True)
             
+        RPIO.wait_for_interrupts(threaded=True)
+
         self._cond = threading.Condition()
         self._tickcount = 0
         self._polltime = 0.1
+        self._startstamp = 0
+        self._stamp = 0
 
     def loop(self):
         prevticks = 0
         prevstamp = 0
-        polltime = self._polltime
+        polltime = None
+        stopstamp = 0
+        startstamp = 0
         while True:
             self._cond.acquire()
             res = self._cond.wait(polltime); res
             ticks = self._tickcount
             ticksdiff = ticks - prevticks            
             if ticksdiff == 0:  # nothing happened since last poll
+                # stopping
                 self._tickcount = 0 # setup tick event
-            now = time.time()
+                stopstamp = self._stamp
+            else:
+                if polltime is None: # starting
+                    startstamp = self._startstamp
+            stamp = self._stamp
             self._cond.release()
 
             if ticksdiff == 0:
-                polltime = None
-                print "stopping"
+                if polltime:
+                    polltime = None
+                    print "stopping (%d ticks in %.3f seconds)" %(ticks, stamp-startstamp)
+                    ticks = 0
             else:
                 if polltime is None: 
                     polltime = self._polltime
-                    print "starting"      
-
-            diff = now - prevstamp
-            print "%.2f (%d)" % (ticksdiff/diff, ticks)
+                    if stopstamp == 0:
+                        print "starting"
+                    else:
+                        print "starting (idle for %%.3f seconds)" %(startstamp-stopstamp)
+                else:
+                    diff = stamp - prevstamp
+                    print "%.2f (%d)" % (ticksdiff/diff, ticks)
             prevticks = ticks
-            prevstamp = now
+            prevstamp = stamp
 
     def on_turn(self, pin, val):
         self._cond.acquire()
+        self._stamp = time.time()
         if self._tickcount == 0:
             self._cond.notifyAll()
+            self._startstamp = self._stamp
         self._tickcount += 1
         self._cond.release()
 

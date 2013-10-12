d528c62eca96954be18d6caeb1dd6bdf6976f518
Sergey Chernikov <schernikov@gmail.com>
2013-10-12 16:47:26 -0700

    improve portability

--- src/server.py
+++ src/server.py
@@ -66,7 +66,7 @@ def main():
                                    (r'/js/(.*)$', tornado.web.StaticFileHandler, {"path": os.path.join(loc, 'js')}),
                                   ], debug=True, cookie_secret=str(uuid.uuid4()))
     
-    app.listen(args.port, address='localhost')
+    app.listen(args.port, address=args.host)
 
     sc = controller.SensorControl()
     def scont():
